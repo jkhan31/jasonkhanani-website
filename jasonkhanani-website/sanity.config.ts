3782da36bdd178306f873f369cbcaca4ea2f9978
Jason Kester Hanani <jasonkhanani@gmail.com>
2025-12-29 10:05:19 +0700

    chore(studio): integrate Unsplash asset source and docs

--- jasonkhanani-website/sanity.config.ts
+++ jasonkhanani-website/sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure' // Note: 'deskTool' is renamed to 'structureTool' in v3/v4 clean templates
 import { visionTool } from '@sanity/vision'
+import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
 import { schemaTypes } from './schemaTypes' // <--- ENSURE THIS MATCHES YOUR FOLDER NAME
 
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'lrta5lyp',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), unsplashImageAsset].filter(Boolean),
 
   schema: {
     types: schemaTypes,
